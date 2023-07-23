7a5277fcf5935bbd5e8623e64b4b66a7509c177b
IbrahimGhasia <ibrahim.ghasia@gmail.com>
2023-07-23 18:07:28 +0530

    Added PESU experience

--- src/components/VerticalTabs.jsx
+++ src/components/VerticalTabs.jsx
@@ -24,9 +24,10 @@ const VerticalTabs = () => {
 								className="text-sm md:text-md font-semibold"
 								href="#tab1"
 							>
-								Blockchain Lead
+								Frontend Developer Intern
 							</a>
 						</li>
+
 						<li
 							className={`p-2 cursor-pointer hover:bg-sky-900 hover:text-emerald-400 border-l-2 ${
 								activeTab === 2
@@ -39,6 +40,21 @@ const VerticalTabs = () => {
 								className="text-sm md:text-md font-semibold"
 								href="#tab2"
 							>
+								Blockchain Lead
+							</a>
+						</li>
+						<li
+							className={`p-2 cursor-pointer hover:bg-sky-900 hover:text-emerald-400 border-l-2 ${
+								activeTab === 3
+									? "bg-sky-900 text-emerald-400 border-l-2 border-emerald-400 duration-300"
+									: "text-gray-300"
+							}`}
+							onClick={() => handleTabClick(3)}
+						>
+							<a
+								className="text-sm md:text-md font-semibold"
+								href="#tab3"
+							>
 								Outgoing Global Volunteer
 							</a>
 						</li>
@@ -49,6 +65,44 @@ const VerticalTabs = () => {
 				<div className="p-2">
 					{activeTab === 1 && (
 						<div id="tab1">
+							<h2 className="text-lg md:text-2xl text-gray-300 font-semibold">
+								Frontend Developer Intern{" "}
+								<span className="text-emerald-400">
+									@PESU Venture Labs, Remote
+								</span>
+							</h2>
+							<h3 className="text-sm text-gray-300">
+								July 2023 – present
+							</h3>
+							<p className="mt-5 text-sm md:text-lg md:text-justify text-gray-300">
+								As a frontend developer intern at PESU I worked
+								with startup ZERU in which I have been actively
+								involved in designing and implementing
+								cutting-edge components for our product using
+								the latest technologies. Leveraging React and
+								Redux, I have played a significant role in
+								crafting intuitive and user-friendly interfaces,
+								ensuring seamless user experiences.
+								Additionally, I have skillfully utilized the Ant
+								Design (antd) component library to expedite
+								development while maintaining a consistent and
+								visually appealing design language across the
+								application.
+								<br />
+								<br />
+								Furthermore, I had the opportunity to work on a
+								captivating landing page for our product, where
+								I integrated 3D libraries like Three.js to
+								create captivating visual elements.
+								Collaborating with the team, I combined React
+								and Tailwind CSS to achieve a highly responsive
+								and interactive landing page that leaves a
+								lasting impression on visitors.
+							</p>
+						</div>
+					)}
+					{activeTab === 2 && (
+						<div id="tab2">
 							<h2 className="text-lg md:text-2xl text-gray-300 font-semibold">
 								Blockchain Lead{" "}
 								<span className="text-emerald-400">
@@ -69,8 +123,8 @@ const VerticalTabs = () => {
 							</p>
 						</div>
 					)}
-					{activeTab === 2 && (
-						<div id="tab2">
+					{activeTab === 3 && (
+						<div id="tab3">
 							<h2 className="text-lg md:text-2xl text-gray-300 font-semibold">
 								Outgoing Global Volunteer{" "}
 								<span className="text-emerald-400">
